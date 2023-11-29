08f6017f4b4110cbcebf4163d6a9998a529e2a21
BenFischer001 <benfischer52@gmail.com>
2023-11-28 21:32:43 -0600

    add: button class

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import Header from "./components/Header";
 
 function App() {
-  const x = false
   return (
     <div className="App">
       <Header title = 'Hello'></Header>
-      <h1>Hello {x ? 'True': 'False'}</h1>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 import PropTypes from 'prop-types'
+import Button from './Button'
 
 const Header = ({title}) => {
   return (
-    <header>
-        Header {title}
+    <header className='header'>
+
+        <h1>{title}</h1>
+        <Button color="red" text="Add" />
     </header>
   )
 }
